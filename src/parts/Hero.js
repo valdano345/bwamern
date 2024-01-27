3568c9f7e9eefadaa7ed049b12b1c8d386c961d6
valdano345 <valdanoesnaidar@gmail.com>
2024-01-28 03:16:27 +0700

    header sudah kelar semua

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -19,32 +19,27 @@ export default function Hero(props) {
     return (
         <section className="container pt-4">
             <div className="row align-items-center">
-                <div className="col-auto pr-5" style={{width: 422}}>
-                    <h1 className="h2 font-weight-bold line-height-1 mb-3">
+                <div className="col-auto pr-5" style={{width: 530}}>
+                    <h1 className="font-weight-bold line-height-1 mb-3">
                         Forget Busy Work, <br />
                         Start Next Vacation
                     </h1>
-                    <p className="mb-5 font-weight-light text-gray-500 w-75">
+                    <p className="mb-5 font-weight-light text-gray-500 w-75" style={{ lineHeight: "170%"}}>
                     We provide what you need to enjoy your 
                     holiday with family. Time to make another 
                     memorable moments.
                     </p>
                     <Button className="btn px-5" hasShadow isPrimary onClick={showMostPicked}>
-                        Show Me
-                    </Button>
-                    {/* <Button className="btn px-5" hasShadow isPrimary  type="link" href="">
-                        Home
-                    </Button> */}
-                    
-
+                        Show Me Now
+                    </Button>                    
                     <div className="row mt-5">
-                        <div className="col-auto">
+                        <div className="col-auto" style={{marginRight: 50}}>
                             <img width="36" height="36" src={IconTraveler} alt={`${props.data.travelers} Travelers`}/>
                             <h6 className="mt-3">
                                 {props.data.travelers} <span className="text-gray-500 font-weight-light">Travelers</span>
                             </h6>
                         </div>
-                        <div className="col-auto">
+                        <div className="col-auto" style={{marginRight: 50}}>
                             <img width="36" height="36" src={IconTreasure} alt={`${props.data.treasures} Treasures`}/>
                             <h6 className="mt-3">
                                 {props.data.treasures} <span className="text-gray-500 font-weight-light">Treasures</span>
@@ -56,6 +51,13 @@ export default function Hero(props) {
                                 {props.data.cities} <span className="text-gray-500 font-weight-light">Cities</span>
                             </h6>
                         </div>
+                    </div>
+                </div>
+
+                <div className="col-6 pl-5">
+                    <div style={{width: 520, height: 410}}>
+                        <img src={ImageHero} alt="Room with couches" className="img-fluid position-absolute" style={{margin: "-30px 0 0 -30px", zIndex: 1}} />
+                        <img src={ImageHero_} alt="Room with couches frame" className="img-fluid position-absolute" style={{margin: "1 15px 15px 0"}} />
                     </div>
                 </div>
             </div>      
