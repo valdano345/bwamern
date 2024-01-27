d86f330ee2c2f84be0fbcadebeccbae6151a1ac3
valdano345 <valdanoesnaidar@gmail.com>
2024-01-28 06:51:45 +0700

    Most Picked sudah kelar

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react'
 
 import Hero from "../parts/Hero"
 import Header from "../parts/Header"
+import MostPicked from "../parts/MostPicked"
 import landingPage from '../json/landingPage.json'
 
 export default class LandingPage extends Component{
+  constructor(props){
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     console.log(this.props)
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero data={landingPage.hero} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
       </>
     )
   }
