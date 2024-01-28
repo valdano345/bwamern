ba62bb5225db59b17b4120fcad1fb88ed267d75d
valdano345 <valdanoesnaidar@gmail.com>
2024-01-29 05:24:24 +0700

    finish input date

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 
-import InputNumber from "../elements/Form/InputNumber"
+import InputDate from "../elements/Form/InputDate"
 
 export default class Example extends Component{
     state = {
-        value: "1"
+        value: {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: "selection",
+        }
+        
     }
     
     handleChange = e => {
-        this.setState({[e.target.name]: e.target.value})
+        this.setState({ [e.target.name]: e.target.value });
     }
     
     render() {
@@ -16,14 +21,12 @@ export default class Example extends Component{
             <div className="container">
                 <div className="row align-items-center justify-content-center" style={{height: "100vh"}}>
                     <div className="col-auto">
-                        <InputNumber 
-                            max={30}
-                            isSuffixPlural
-                            suffix=' night'
-                            onChange={this.handleChange}
-                            name="value"
-                            value={this.state.value}
-                        />
+                    <InputDate
+                        max={30}
+                        onChange={this.handleChange}
+                        name="value"
+                        value={this.state.value}
+                    />
                     </div>
                 </div>
             </div>
